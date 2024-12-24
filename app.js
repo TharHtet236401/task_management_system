db69e93cc7a645ce8792a81997367165fef6ea20
TharHtet236401 <tharhteta3@gmail.com>
2024-12-25 01:55:24 +0700

    Implement rate limiting for user API: add express-rate-limit middleware to restrict requests, enhancing security and performance. Update package.json and package-lock.json to include express-rate-limit dependency.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import connectMongoDB from "./config/connectMongoDB.js";
 dotenv.config();
 
@@ -12,7 +13,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/user", userRoute);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later."
+});
+
+app.use("/api/user", limiter, userRoute);
 app.listen(PORT, () => {
   connectMongoDB();
   console.log(`Server is running on port ${PORT}`);
